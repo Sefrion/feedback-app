96181062c87edea15e58451186d0af1f96276e95
Sefrion <ilyenkov96@gmail.com>
2023-12-28 11:45:21 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
+import AboutIconLink from './components/AboutIconLink';
+import About from './pages/About';
+import { FeedbackProvider } from './context/FeedbackContext';
+
+function App() {
+	return (
+		<FeedbackProvider>
+			<Router>
+				<Header />
+				<div className='container'>
+					<Routes>
+						<Route
+							path='/'
+							element={
+								<>
+									<FeedbackForm />
+									<FeedbackStats />
+									<FeedbackList />
+								</>
+							}
+						></Route>
+						<Route path='/about' element={<About />} />
+					</Routes>
+				</div>
+				<AboutIconLink />
+			</Router>
+		</FeedbackProvider>
+	);
+}
+
+export default App;
