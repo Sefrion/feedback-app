affea14ce40e3966cbc4630f29fa3a63e020b5d8
Sefrion <ilyenkov96@gmail.com>
2023-12-29 21:49:53 +0200

    Final commit

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,6 +1,5 @@
 import { createContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
@@ -19,7 +18,7 @@ export const FeedbackProvider = ({ children }) => {
 	// Fetch feedback
 	const fetchFeedback = async () => {
 		const response = await fetch(
-			'http://localhost:5000/feedback?_sort=id&_order=asc'
+			'http://localhost:5000/feedback?_sort=id&_order=desc'
 		);
 		const data = await response.json();
 
@@ -29,14 +28,25 @@ export const FeedbackProvider = ({ children }) => {
 	};
 
 	// Add Feedback
-	const addFeedback = (newFeedback) => {
-		newFeedback.id = uuidv4();
-		setFeedback([newFeedback, ...feedback]);
+	const addFeedback = async (newFeedback) => {
+		const response = await fetch('http://localhost:5000/feedback', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(newFeedback),
+		});
+
+		const data = await response.json();
+
+		setFeedback([data, ...feedback]);
 	};
 
 	// Delete feedback
-	const deleteFeedback = (id) => {
+	const deleteFeedback = async (id) => {
 		if (window.confirm('Are you sure you want to delete?')) {
+			await fetch(`http://localhost:5000/feedback/${id}`, { method: 'DELETE' });
+
 			setFeedback(feedback.filter((item) => item.id !== id));
 		}
 	};
@@ -50,10 +60,20 @@ export const FeedbackProvider = ({ children }) => {
 	};
 
 	// Updated feedback
-	const updateFeedback = (id, updItem) => {
+	const updateFeedback = async (id, updItem) => {
+		const response = await fetch(`http://localhost:5000/feedback/${id}`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(updItem),
+		});
+
+		const data = await response.json();
+
 		setFeedback(
 			feedback.map((item) => {
-				item.id === id ? { ...item, ...updItem } : item;
+				item.id === id ? { ...item, ...data } : item;
 			})
 		);
 	};
