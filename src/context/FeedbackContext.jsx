8b163c39909e2533762a912f0e05ea95ad117fd2
Sefrion <ilyenkov96@gmail.com>
2023-12-29 19:07:11 +0200

    Json server and fetchFeedback were added

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,28 +1,47 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
-import FeedbackData from '../data/FeedbackData';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-	const [feedback, setFeedback] = useState(FeedbackData);
+	const [isLoading, setIsLoading] = useState(true);
+	const [feedback, setFeedback] = useState([]);
 	const [feedbackEdit, setFeedbackEdit] = useState({
 		item: {},
 		edit: false,
 	});
 
+	useEffect(() => {
+		fetchFeedback();
+	}, []);
+
+	// Fetch feedback
+	const fetchFeedback = async () => {
+		const response = await fetch(
+			'http://localhost:5000/feedback?_sort=id&_order=asc'
+		);
+		const data = await response.json();
+
+		setFeedback(data);
+
+		setIsLoading(false);
+	};
+
+	// Add Feedback
 	const addFeedback = (newFeedback) => {
 		newFeedback.id = uuidv4();
 		setFeedback([newFeedback, ...feedback]);
 	};
 
+	// Delete feedback
 	const deleteFeedback = (id) => {
 		if (window.confirm('Are you sure you want to delete?')) {
 			setFeedback(feedback.filter((item) => item.id !== id));
 		}
 	};
 
+	// Set feedback to edit mode
 	const editFeedback = (item) => {
 		setFeedbackEdit({
 			item,
@@ -30,6 +49,7 @@ export const FeedbackProvider = ({ children }) => {
 		});
 	};
 
+	// Updated feedback
 	const updateFeedback = (id, updItem) => {
 		setFeedback(
 			feedback.map((item) => {
@@ -43,6 +63,7 @@ export const FeedbackProvider = ({ children }) => {
 			value={{
 				feedback,
 				feedbackEdit,
+				isLoading,
 				deleteFeedback,
 				addFeedback,
 				editFeedback,
